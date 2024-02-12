a9ab54d3e1884285611f890ee5ffd78923daba27
Bruno Locha <bruno@worldscript.com.br>
2024-02-12 00:34:12 -0300

    Created simple camera follow script

--- HyperCasual/Assets/MyProject/Scripts/Utils/CameraMoviment.cs
+++ HyperCasual/Assets/MyProject/Scripts/Utils/CameraMoviment.cs
@@ -8,5 +8,22 @@ namespace Project.Utils
     {
         [SerializeField]
         private Transform target;
+        [SerializeField]
+        private float distanceYFromTarget = 11f;
+        [SerializeField]
+        private float distanceZFromTarget = -11f;
+
+
+        private void Update()
+        {
+            if (target == null)
+                return;
+
+            transform.position = new Vector3(
+                target.position.x,
+                target.position.y + distanceYFromTarget,
+                target.position.z + distanceZFromTarget
+                );
+        }
     }
 }
